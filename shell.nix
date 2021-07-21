e3e50b409d318b8aba8ae81941241da1ebfd03e3
Jayesh Bhoot <jayesh@bhoot.sh>
2021-07-21 22:34:42 +0530

    Add `ocamlPackages.findlib` as a dependency. Without it, `ocamlfind` not found in the PATH, without which `dune` does not see any of the installed dependencies referred in `dune` config.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     ocaml
+    ocamlPackages.findlib
     dune_2
+    ocamlPackages.ocaml-lsp
+    ocamlformat
     ocamlPackages.utop
+
+    ocamlPackages.re
+
     mpv
   ];
 }
